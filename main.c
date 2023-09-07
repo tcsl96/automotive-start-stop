cefc855fb996841c71671501e07655bcb41712d8
Klaus Damasceno <77865111+kmdamasceno@users.noreply.github.com>
2023-09-06 22:07:25 -0300

    Commit da função main]

--- main.c
+++ main.c
@@ -2,14 +2,14 @@
 #include <stdbool.h>
 
 bool buttonPressed, seatbeltSensor, obsSensor, hoodSensor, batteryStatus, speedStatus;
-bool brakeStatus, obsStatus, seatbeltStatus, hoodStatus, tempCheck, batteryCheck, brakeCheck, startStopStatus, speedCheck;
+bool brakeStatus, obsStatus, seatbeltStatus, hoodStatus, tempCheck, batteryCheck, brakeCheck, startStopStatus, speedCheck, hardwareTest, systemTest;
 float cyc_Mph, fc_Temp, ess_SOC_hist, brake_torque, cyc_Kph;
 
 // Declaring simulink functions developed for the model
 
 float acquireSpeed(cyc_Mph)
 {
-    cyc_Kph = cyc_Mph*1.6095;
+    cyc_Kph = cyc_Mph * 1.6095;
     return(cyc_Kph);
 
     if (cyc_Kph > 5)
@@ -43,4 +43,34 @@ bool acquireBrakeUsage(brake_torque)
         brakeCheck = false;
 }
 
-// Running the main function, which checks if all variables are true
+bool testHardware(batteryStatus, speedStatus, brakeStatus, obsStatus, seatbeltStatus, hoodStatus)
+{
+    if (batteryStatus && speedStatus && brakeStatus && obsStatus && seatbeltStatus && hoodStatus == true)
+        hardwareTest = true;
+    else
+        hardwareTest = false;
+}
+
+bool testSystem(buttonPressed, seatbeltSensor, obsSensor, hoodSensor, tempCheck, batteryCheck, brakeCheck, speedCheck)
+{
+    acquireSpeed(cyc_Mph);
+    acquireTemperature(fc_Temp);
+    acquireBatteryCharge(ess_SOC_hist);
+    acquireBrakeUsage(brake_torque);
+
+    if (buttonPressed && seatbeltSensor && obsSensor && hoodSensor && tempCheck && batteryCheck && brakeCheck && speedCheck == true)
+        systemTest == true;
+    else
+        systemTest == false;
+}
+
+int main()
+{
+    testHardware(batteryStatus, speedStatus, brakeStatus, obsStatus, seatbeltStatus, hoodStatus);
+    testSystem(buttonPressed, seatbeltSensor, obsSensor, hoodSensor, tempCheck, batteryCheck, brakeCheck, speedCheck);
+
+    if (testHardware && testSystem == true)
+        startStopStatus = true;
+    else
+        startStopStatus = false;
+}
